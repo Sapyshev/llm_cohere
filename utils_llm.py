cc9dab5d99e748b27129931f7a949f2882f34933
Rollan Sapyshev <rollan.sapyshev@qdata.kz>
2023-08-18 13:26:51 +0600

    rerank file

--- utils_llm.py
+++ utils_llm.py
@@ -8,6 +8,7 @@ def print_result(result):
         print()
 
 def keyword_search(query,
+                   client,
                    results_lang='en',
                    properties = ["title","url","text"],
                    num_results=3):
@@ -31,3 +32,28 @@ def keyword_search(query,
     result = response['data']['Get']['Articles']
     return result
 
+def dense_retrieval(query, 
+                    results_lang='en', 
+                    properties = ["text", "title", "url", "views", "lang", "_additional {distance}"],
+                    num_results=5):
+
+    nearText = {"concepts": [query]}
+    
+    # To filter by language
+    where_filter = {
+    "path": ["lang"],
+    "operator": "Equal",
+    "valueString": results_lang
+    }
+    response = (
+        client.query
+        .get("Articles", properties)
+        .with_near_text(nearText)
+        .with_where(where_filter)
+        .with_limit(num_results)
+        .do()
+    )
+
+    result = response['data']['Get']['Articles']
+
+    return result
